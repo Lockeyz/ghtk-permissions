53c994dd83b41a49bbe4b5486bd1223496c013f0
ducloc <locdivine@gmail.com>
2024-07-29 19:08:31 +0700

    completely

--- app/src/main/java/bdl/lockey/ghtk_permissions/MainActivity.kt
+++ app/src/main/java/bdl/lockey/ghtk_permissions/MainActivity.kt
@@ -4,57 +4,100 @@ import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import bdl.lockey.ghtk_permissions.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var tvPermissionStatus: TextView
-    private lateinit var tvLatitude: TextView
-    private lateinit var tvLongitude: TextView
-    private lateinit var btnGetLocation: Button
+
+    companion object {
+        private const val LOCATION_PERMISSION_CODE = 100
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        tvPermissionStatus = findViewById(R.id.tvPermissionStatus)
-        tvLatitude = findViewById(R.id.tvLatitude)
-        tvLongitude = findViewById(R.id.tvLongitude)
-        btnGetLocation = findViewById(R.id.btnGetLocation)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), 1)
-        } else {
-            tvPermissionStatus.text = "Trạng thái cấp quyền: Được cho phép"
-            btnGetLocation.isEnabled = true
-        }
-
-        btnGetLocation.setOnClickListener {
-            getLocation()
+        binding.btnGetLocation.setOnClickListener {
+            checkPermission(ACCESS_FINE_LOCATION, LOCATION_PERMISSION_CODE)
         }
 
     }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        when {
+            // Xay ra khi quyen truy cap da duoc bat san
+            ContextCompat.checkSelfPermission(this, permission)
+                    == PackageManager.PERMISSION_GRANTED
+            -> {
+                binding.tvPermissionStatus.text = "Trạng thái cấp quyền: Được cho phép"
+                Snackbar.make(
+                    binding.root,
+                    R.string.location_permission_available,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+                getLocation()
+            }
+
+            // Xay ra khi truoc do duoc yeu cau cap quyen nhung bi tu choi
+            // va khong chon "Khong bao gio hoi lai"
+            // Tu choi cap quyen do shouldShowRequest... goi len
+            // thi sau nay showShouldRequest... luon tra ve false = "Khong bao gio hoi lai"
+            // va se luon chay vao nhanh else de tu choi cap quyen
+            ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+            -> {
+                // Dua ra ly do de bat quyen truy cap
+                Snackbar.make(
+                    binding.root, R.string.location_access_required,
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(R.string.ok, View.OnClickListener { // Request the permission
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(ACCESS_FINE_LOCATION),
+                        LOCATION_PERMISSION_CODE
+                    )
+                }).show()
+            }
+            // Xay ra chi khi lan dau xin cap quyen
+            // Khi khong chay check shouldShowRequest... do tu choi cap quyen va khong hoi lai,
+            // se chay vao else nhung se khong hien thi yeu cau cap quyen
+            // va tu choi luon viec cap quyen
+            else -> {
+                Snackbar.make(binding.root, R.string.location_unavailable, Snackbar.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(permission),
+                    requestCode
+                )
+            }
+
+        }
+    }
+
     // ACCESS_FINE_LOCATION
     private fun getLocation() {
         if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
+            == PackageManager.PERMISSION_GRANTED
+        ) {
             fusedLocationClient.lastLocation
                 .addOnSuccessListener { location: Location? ->
                     if (location != null) {
-                        tvLatitude.text = "Vĩ độ: ${location.latitude}"
-                        tvLongitude.text = "Kinh độ: ${location.longitude}"
+                        binding.tvLatitude.text = "Vĩ độ: ${location.latitude}"
+                        binding.tvLongitude.text = "Kinh độ: ${location.longitude}"
                     } else {
                         Toast.makeText(this, "Không thể lấy vị trí", Toast.LENGTH_SHORT).show()
                     }
@@ -62,16 +105,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1) {
+        if (requestCode == LOCATION_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                tvPermissionStatus.text = "Trạng thái cấp quyền: Được cho phép"
-                btnGetLocation.isEnabled = true
-            } else {
-                tvPermissionStatus.text = "Trạng thái cấp quyền: Bị từ chối"
-                btnGetLocation.isEnabled = false
+                binding.tvPermissionStatus.text = "Trạng thái cấp quyền: Được cho phép"
+                binding.btnGetLocation.isEnabled = true
+
+                // Thong bao quyen duoc cap
+                Snackbar.make(
+                    binding.root, R.string.location_permission_granted,
+                    Snackbar.LENGTH_SHORT
+                )
+                    .show()
+                getLocation()
+            } else { // Xay ra khi xin cap quyen bi tu choi
+                binding.tvPermissionStatus.text = "Trạng thái cấp quyền: Bị từ chối"
+                binding.btnGetLocation.isEnabled = false
+
+                // Thong bao quyen bi tu choi
+                Snackbar.make(
+                    binding.root, R.string.location_permission_denied,
+                    Snackbar.LENGTH_SHORT
+                ).show()
             }
+        } else {
+            // Ignore all other requests.
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
